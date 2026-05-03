44c3bdf7698221507d1e58aae51fdb7e2c37c941
sambad-K <sambadkhatiwada939@gmail.com>
2026-05-03 13:26:51 +0545

    Delete Learn mod/Front/dance-eval-test-ui directory

--- Learn mod/Front/dance-eval-test-ui/src/index.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import AppWrapper from "./AppWrapper.jsx";
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<AppWrapper />);
